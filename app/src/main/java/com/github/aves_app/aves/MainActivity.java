d4f6d05f7356c820d0ca083a11ae1182b03e68b7
Tiim <tim.bachmann@cad.ch>
2016-10-23 19:32:45 +0200

    Add android studio project

---
+++ app/src/main/java/com/github/aves_app/aves/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.aves_app.aves;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
